325ce838d0af33727b11e876817f2ae665ffa478
Kağan Koç <cemsukagan@hotmail.com>
2020-04-23 16:53:33 +0300

    Updates

---
+++ precache-manifest.c17146e8ed936e80ac27b7df1a7e62a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d28f5e29a9b4a22a068fad496427a9e",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "d8eaae30902d7bb4e143",
+    "url": "/rolodex/static/css/main.b851c5a0.chunk.css"
+  },
+  {
+    "revision": "3c9d29464f6976dfc6d0",
+    "url": "/rolodex/static/js/2.eeae8fd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolodex/static/js/2.eeae8fd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8eaae30902d7bb4e143",
+    "url": "/rolodex/static/js/main.c89e4b7d.chunk.js"
+  },
+  {
+    "revision": "3727d9797b52f86e3208",
+    "url": "/rolodex/static/js/runtime-main.691a164e.js"
+  }
+]);
